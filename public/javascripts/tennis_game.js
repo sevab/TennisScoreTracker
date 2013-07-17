f40cd9f8b2707f969cbe87ec57ef54c75147bc02
Seva <vabaskin@gmail.com>
2013-07-17 16:11:39 +0100

    Fixed a bug where ADV-40 wouldn't work; website's working; NEXT: add games scoring

--- public/javascripts/tennis_game.js
+++ public/javascripts/tennis_game.js
@@ -16,14 +16,16 @@ function Game(player1, player2, gender) {
 	this.gender = gender;
 	
 	var PAIRS = {
+		'0' : 'Love',
 		'1' : '15',
 		'2' : '30',
-		'3' : '40',
+		'3' : '40'
 	}
 
 	this.isTie = function() {
 		return this.player1.getPoints() == this.player2.getPoints();
 	}
+
 
 	this.getScore = function(player) {
 		if (Math.abs(this.player2.getPoints()-this.player1.getPoints()) > 1) {
@@ -31,25 +33,31 @@ function Game(player1, player2, gender) {
 				return "game";
 			};
 		};
-
-		if (this.player1.getPoints() >= 3 && this.player2.getPoints() >= 3) {
-			var a = Math.abs(this.player2.getPoints()-this.player1.getPoints());
-
-			if (a == 1) {
-				return "ads" + '-' + "40";
-			};
-		};		
-
 		if (this.isTie() && this.player1.getPoints() > 2) {
 			return "deuce";
 		};
-		return PAIRS[player.getPoints().toString()] + '-' + PAIRS[this.opponent(player).getPoints().toString()];
+
+		if (this.player1.getPoints() >= 3 && this.player2.getPoints() >= 3) {
+			var a = player.getPoints()-this.opponent(player).getPoints();
+			if (Math.abs(a) == 1) {
+				if (a > 0) {
+					return "ADV";
+				} else {
+					return "40";
+				};
+			};
+		};		
+
+		return PAIRS[player.getPoints().toString()];
 	}
+
 
 	this.win = function(player) {
 		player.winPoint();
 		return this.getScore(player);
 	}
+
+
 
 	this.opponent = function(player) {
 		if(player == this.player1) {
