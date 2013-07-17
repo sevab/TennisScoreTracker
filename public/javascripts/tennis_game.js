8cd1ba51f1214622286ea913b9eaed9dfd80c018
Seva <vabaskin@gmail.com>
2013-07-17 18:29:02 +0100

    Added Games and Sets functionality; everything seems to work; added keeping track of past games functionality; NEXT: Matches based on gender; display of history column; start form to input players names and gender

--- public/javascripts/tennis_game.js
+++ public/javascripts/tennis_game.js
@@ -1,6 +1,8 @@
 function Player() {
 	this.name = 'Alex';
 	this.points = 0;
+	this.gamesWon = 0;
+	this.setsWon = 0;
 
 	this.getPoints  = function() {
 		return this.points;
@@ -8,12 +10,37 @@ function Player() {
 	this.winPoint = function() {
 		this.points = this.points + 1;
 	}
+
+	this.winGame = function() {
+		this.gamesWon = this.gamesWon + 1;
+	}
+	this.getGames  = function() {
+		return this.gamesWon;
+	}
+
+	this.resetPoints  = function() {
+		this.points = 0;
+	}
+
+	this.winSet = function() {
+		this.setsWon = this.setsWon + 1;
+	}
+	this.getSets  = function() {
+		return this.setsWon;
+	}
+
+	this.resetGames = function() {
+		this.gamesWon = 0;
+	}
+
+
 };
 
 function Game(player1, player2, gender) {
 	this.player1 = player1;
 	this.player2 = player2;
 	this.gender = gender;
+	this.gamesArchive = new Array();
 	
 	var PAIRS = {
 		'0' : 'Love',
@@ -28,11 +55,25 @@ function Game(player1, player2, gender) {
 
 
 	this.getScore = function(player) {
-		if (Math.abs(this.player2.getPoints()-this.player1.getPoints()) > 1) {
+		// if (Math.abs(this.player2.getPoints()-this.player1.getPoints()) > 1) {
+		// 	if (this.player1.getPoints() > 3 || this.player2.getPoints() > 3) {
+		// 		return "game";
+		// 	};
+		// };
+
+		var b = player.getPoints()-this.opponent(player).getPoints();
+		if (Math.abs(b) > 1) {
 			if (this.player1.getPoints() > 3 || this.player2.getPoints() > 3) {
-				return "game";
+				if (b > 0) {
+					this.winGame(player);
+					return "game";
+				} else {
+					return "game";
+				};
 			};
 		};
+
+
 		if (this.isTie() && this.player1.getPoints() > 2) {
 			return "deuce";
 		};
@@ -66,6 +107,46 @@ function Game(player1, player2, gender) {
 		else {
 			return this.player1;
 		}
-
 	}
+
+	this.winGame = function(player) {
+		player.winGame();
+		this.isSetWon(player);
+		player.resetPoints();
+		this.opponent(player).resetPoints();
+	}
+
+
+	this.isSetWon = function(player) {
+		if (player.getGames() == 6) {
+
+			if (this.opponent(player).getGames() == 6) {
+				return true
+			};
+
+			if (this.opponent(player).getGames() < 5) {
+				this.winSet(player);
+			};
+		};
+
+		if (player.getGames() == 7) {
+			this.winSet(player);
+		}
+	}
+
+	this.winSet = function(player) {
+		player.winSet();
+		this.archiveGames();
+		player.resetGames();
+		this.opponent(player).resetGames();
+	}
+
+	this.archiveGames = function() {
+		var x = this.player1.getGames();
+
+		this.gamesArchive.push({
+			x: this.player2.getGames()
+		})
+	}
+
 };
